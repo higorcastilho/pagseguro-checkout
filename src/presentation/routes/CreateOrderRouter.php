4a82a90f7379515502e4a49116fb699156e9c263
Higor Castilho <castilhohf@gmail.com>
2021-01-12 17:14:43 -0300

    test: ensure CreateOrderRouterTest returns 400 if any param is not provided

--- src/presentation/routes/CreateOrderRouter.php
+++ src/presentation/routes/CreateOrderRouter.php
@@ -1,15 +1,31 @@
 <?php
 namespace PagseguroService\presentation\routes;
-
+ini_set("display_errors", "on");
 class CreateOrderRouter {
 
-	/*private $createOrderUseCase;
-
-	public function __construct(CreateOrderUseCase $createOrderUseCase) {
-		$this->createOrderUseCase = $createOrderUseCase;
-	}*/
-
 	public function route ($input) {
-		echo $input;
+			$data = json_decode($input, true);
+			
+			if (
+				!$data['itemId1'] ||
+				!$data['itemAmount1'] ||
+				!$data['itemQuantity1'] ||
+				!$data['senderName'] ||
+				!$data['senderAreaCode'] ||
+				!$data['senderPhone'] ||
+				!$data['shippingAddressStreet'] ||
+				!$data['shippingAddressNumber'] ||
+				!$data['shippingAddressComplement'] ||
+				!$data['shippingAddressDistrict'] ||
+				!$data['shippingAddressPostalCode'] ||
+				!$data['shippingAddressCity'] ||
+				!$data['shippingAddressState']
+			) {
+				http_response_code(400);
+				return ;
+			}
+
+			http_response_code(200);
 	}
+
 }
--- tests/presentation/routes/CreateOrderRouterTest.php
+++ tests/presentation/routes/CreateOrderRouterTest.php
@@ -23,18 +23,32 @@ class CreateOrderRouterTest extends TestCase {
 	
 	use MakeSut;
 
-	public function testEnsureCreateOrderRouterReceivesPostDataCorrectly (): void {
-		
-		list($sut) = $this->makeSut();
+	public function testShouldReturn400IfNoParamIsProvided () {
 		
 		$url = "http://localhost:8000/";
-
 		$client = new GuzzleHttp\Client(['base_uri' => $url]);
+		$json = [
+			'itemId1' => '',
+			'itemAmount1' => 'any_amount',
+			'itemQuantity1' => 'any_quantity',
+			'senderName' => 'any_name',
+			'senderAreaCode' => 'any_area_code',
+			'senderPhone' => 'any_phone',
+			'shippingAddressStreet' => 'any_shipping_address',
+			'shippingAddressNumber' => 'any_address_number',
+			'shippingAddressComplement' => 'any_address_complement',
+			'shippingAddressDistrict' => 'any_address_district',
+			'shippingAddressPostalCode' => 'any_address_postal_code',
+			'shippingAddressCity' => 'any_address_city',
+			'shippingAddressState' => 'any_address_state'
+		];
 
-		$response = $client->request('POST', '/pagseguro-checkout/src/main', [
-			'json' => ['foo' => 'bar']
-		]);
+		$response = $client->request(
+			'POST', 
+			'/pagseguro-checkout/src/main/createOrder/create', 
+			['json' => $json, 'http_errors' => false ]
+		);	
 
-		echo $response->getBody(true);
-	}	
+		$this->assertEquals(400, $response->getStatusCode());
+	}
 }
