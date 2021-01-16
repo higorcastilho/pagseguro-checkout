5fd6b8ec10c2d9a7ed9fae13ca08c9e8b36b6bee
Higor Castilho <castilhohf@gmail.com>
2021-01-16 18:06:28 -0300

    refactor: move headers settings to their own file

--- src/main/config/App.php
+++ src/main/config/App.php
@@ -9,13 +9,6 @@ class App {
 	
 	public function start ($requestURI) {
 
-		header("Access-Control-Allow-Origin: *");
-		header("Content-Type: application/json; charset=UTF-8");
-		header("Access-Control-Allow-Methods: POST");
-		header("Access-Control-Max-Age: 3600");
-		header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-
 		$this->uri = parse_url($requestURI, PHP_URL_PATH);
 		$this->uri = explode('/', $this->uri);
 
--- src/main/index.php
+++ src/main/index.php
@@ -1,8 +1,8 @@
 <?php
 ini_set("display_errors", "on");
-
 use PagseguroService\main\config\App;
 
+require_once "./middlewares/headers.php";
 require_once './routes/CreateOrderRoutes.php';
 require_once './composers/CreateOrderRouterComposer.php';
 require_once './adapters/Adapter.php';
